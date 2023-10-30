5056585562b25064680730c7fbf78d6d738377f3
Pratik Saxena <pratik2709@gmail.com>
2023-10-31 10:37:39 +1300

    intermediate for fastapi

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 # main.py
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World1"}
+
+
+@app.post("/item")
+async def create_item(item: Item):
+    return item
