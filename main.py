0eebd05dd614a775541da1d802ceee8db89d936d
Pratik Saxena <pratik2709@gmail.com>
2023-11-01 10:24:30 +1300

    cleanups

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-# main.py
-import multiprocessing
-
-import redis as redis
-from fastapi import FastAPI, HTTPException, Depends, BackgroundTasks
-from sqlalchemy import text
-from sqlalchemy.orm import Session
-
-# Dependency
-from database import SessionLocal
-from schemas import Item
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -25,6 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World1"}
-
-
-
--- utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-import sqlalchemy as sa
-
-class Timestamp(object):
-    created = sa.Column(sa.DateTime, default=datetime.utcnow, nullable=False, index=True)
-    updated = sa.Column(sa.DateTime, default=datetime.utcnow, nullable=False)
