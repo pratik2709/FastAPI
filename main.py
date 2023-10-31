755110ed6c44a2c296cd3f213581e1e68f2ccc58
Pratik Saxena <pratik2709@gmail.com>
2023-11-01 11:13:15 +1300

    intermediate for uploading into static folder

--- main.py
+++ main.py
@@ -25,17 +25,13 @@ class DeviceConfig(BaseModel):
     device_id: str
 
 
-@app.post("/device-configurations/")
-async def create_configuration(app_config: UploadFile = File(...),
+@app.post("/device-configurations/{device_id}/")
+async def create_configuration(device_id: str, app_config: UploadFile = File(...),
                                depth_config: UploadFile = File(...)):
     try:
-        # device_id = data.device_id
-        app_config_path = save_file_to_static_folder(app_config, f"{1}_app_config.yaml")
-        depth_config_path = save_file_to_static_folder(depth_config, f"{1}_depth.yaml")
+        app_config_path = save_file_to_static_folder(app_config, f"{device_id}_app_config.yaml")
+        depth_config_path = save_file_to_static_folder(depth_config, f"{device_id}_depth.yaml")
 
-        print("over here")
-        print(app_config_path)
-        print(depth_config_path)
         # create_device_configuration(device_id, app_config_path, depth_config_path)
         return {"message": "Configuration created successfully"}
     except Exception as e:
