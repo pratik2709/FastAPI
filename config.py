819d9d6ff4f2c8b30c7922379c518c43a3377ad5
Pratik Saxena <pratik2709@gmail.com>
2023-11-01 13:08:47 +1300

    added authentication

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
 from decouple import config
+from fastapi.security import APIKeyHeader
+
 
 class Settings:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_DIR = os.path.join(BASE_DIR, "static")
-
+    API_KEY_NAME = "X-API-KEY"
+    api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 settings = Settings()
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.params import File, Depends
 from sqlalchemy.orm import Session
 
+from config import settings
 from database import get_db
 from src.resource.utils import save_file_to_static_folder, create_device_configuration
 
 app = FastAPI()
+
+load_dotenv()
+
+def get_api_key(api_key_header: str = Depends(settings.api_key_header)):
+    valid_api_keys = os.environ.get("VALID_API_KEYS", "").split(",")
+    if api_key_header in valid_api_keys:
+        return api_key_header
+    raise HTTPException(status_code=400, detail="Invalid API key")
 
 
 @app.get("/ping")
@@ -16,7 +28,8 @@ async def root():
 @app.post("/device-configurations/{device_id}/")
 async def create_configuration(device_id: str, app_config: UploadFile = File(...),
                                depth_config: UploadFile = File(...),
-                               db: Session = Depends(get_db)):
+                               db: Session = Depends(get_db),
+                               api_key: str = Depends(get_api_key)):
     try:
         app_config_path = save_file_to_static_folder(app_config, f"{device_id}_app_config.yaml")
         depth_config_path = save_file_to_static_folder(depth_config, f"{device_id}_depth.yaml")
